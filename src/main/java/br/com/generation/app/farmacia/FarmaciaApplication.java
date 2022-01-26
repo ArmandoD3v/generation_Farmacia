f8179cf383efa5158e97ba22727810416b0245e1
Matheus <matheus.mfa10@gmail.com>
2022-01-26 11:08:05 -0300

    @Matheus-Anjos [Task : Criação do Projeto]

---
+++ src/main/java/br/com/generation/app/farmacia/FarmaciaApplication.java
@@ -0,0 +1,13 @@
+package br.com.generation.app.farmacia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciaApplication.class, args);
+	}
+
+}
